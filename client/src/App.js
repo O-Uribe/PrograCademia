bd40f038b18f59ae16c39ceb4140931762eb8005
Gonzalo <ouribe2020@alu.uct.cl>
2022-09-01 00:17:53 -0400

    Cambios en implementación anterior
    Se reestructuran los ficheros relacionados al cliente y servidor del proyecto, donde ahora es posible observar a los clientes de forma independiente dentro del servidor.

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:4000');
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola </h1>
+    </div>
+  );
+}
+
+export default App;
