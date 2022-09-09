fd1f773666c8ad6de9945d7209be860909c92e9c
niquiprime <71198628+niquiprime@users.noreply.github.com>
2022-09-09 17:04:44 -0400

    Delete database.js

--- server/DB/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const URI = "mongodb+srv://nico:nico123@cluster0.oxwjx.mongodb.net/?retryWrites=true&w=majority"
-
-mongoose.connect(URI)
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.error(err));
-
-module.exports = mongoose;
