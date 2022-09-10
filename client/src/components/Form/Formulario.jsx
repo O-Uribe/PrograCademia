b271c57e777785207658719d6b43ce2ba4998530
niquiprime <71198628+niquiprime@users.noreply.github.com>
2022-09-09 22:21:15 -0400

    Css muy basico para form con validacion de datos

--- client/src/components/Form/Formulario.jsx
+++ client/src/components/Form/Formulario.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useForm } from "react-hook-form";
 import { contrasena_Repetida } from "./actions/validator";
+import { ErrorMessage } from "@hookform/error-message";
 import styles from "../../css/form.module.css";
     /* 
     
@@ -17,7 +18,7 @@ import styles from "../../css/form.module.css";
     */
 const Formulario =() => {
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
     const onSubmit = (data) => {
         console.log(data);
@@ -28,49 +29,54 @@ const Formulario =() => {
             <h2>Formulario</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div>
-                    <label>Nombre</label>
-                    <input type="text" name="nombre" className={styles.field} {...register("nombre",{
+                    <label className={styles.label} >Nombre</label>
+                    <input type="text" name="nombre" className={styles.input} {...register("nombre",{
                         required: {value:true, message:"Campo obligatorio"},
-                        /*maxLength: {value: 20, message:"Máximo 20 caracteres"}*/
+                        maxLength: {value: 20, message:"Máximo 20 caracteres"}
                     })} />
-                    {/*{errors.nombre?.type === "required" && <p>Hola</p>}*/}
+                    <ErrorMessage 
+                    errors={errors} 
+                    name="nombre"
+                    render={({message}) => 
+                    <p className={styles.error}>{message}</p>}
+                    />
                 </div>
                 <div>
-                    <label>Apellido</label>
-                    <input type="text" name="apellido" className={styles.field} {...register("apellido",{
+                    <label className={styles.label}>Apellido</label>
+                    <input type="text" name="apellido" className={styles.input} {...register("apellido",{
                         required: {value:true, message:"Campo obligatorio"},
                         maxLength: {value: 20, message:"Máximo 20 caracteres"}
                     })}/>
                 </div>
                 <div>
-                    <label>Email</label>
-                    <input type="email" name="email" className={styles.field} {...register("email",{
+                    <label className={styles.label}>Email</label>
+                    <input type="email" name="email" className={styles.input} {...register("email",{
                         required: {value:true, message:"Campo obligatorio"},}
                     )}/>
                 </div>
                 <div>
-                    <label>RUT</label>
-                    <input type="number" name="rut" className={styles.field} {...register("rut",{
+                    <label className={styles.label}>RUT</label>
+                    <input type="number" name="rut" className={styles.input} {...register("rut",{
                         required: {value:true, message:"Campo obligatorio"},
                         maxLength: {value: 9, message:"Máximo 9 caracteres"}
                     })}/>
                 </div>
                 <div>
-                    <label>Contraseña</label>
-                    <input type="password" name="contraseña" className={styles.field} {...register("contraseña",{
+                    <label className={styles.label}>Contraseña</label>
+                    <input type="password" name="contraseña" className={styles.input} {...register("contraseña",{
                         required: {value:true, message:"Campo obligatorio"},
                     })}/>
                 </div>
                 <div>
-                    <label>Repetir contraseña</label>
-                    <input type="password" name="repetirContraseña" className={styles.field} {...register("repetirContraseña",{
+                    <label className={styles.label}>Repetir contraseña</label>
+                    <input type="password" name="repetirContraseña" className={styles.input} {...register("repetirContraseña",{
                         required: {value:true, message:"Campo obligatorio"},
                         /*validate: contrasena_Repetida*/
                     })}/>
                 </div>
                 <div>
-                    <label>Rol</label>
-                    <select name="rol" className={styles.field} {...register("rol",{
+                    <label className={styles.label}>Rol</label>
+                    <select name="rol" className={styles.input} {...register("rol",{
                         required: {value:true, message:"Campo obligatorio"},
                     })}>
                         <option value="profesor">Profesor</option>
@@ -78,13 +84,13 @@ const Formulario =() => {
                     </select>
                 </div>
                 <div>
-                    <label>Año de ingreso</label>
-                    <input type="number" name="añoIngreso" className={styles.field} {...register("añoIngreso",{
+                    <label className={styles.label}>Año de ingreso</label>
+                    <input type="number" name="añoIngreso" className={styles.input} {...register("añoIngreso",{
                         required: {value:true, message:"Campo obligatorio"},
                     })}/>
                 </div>
                 <div>
-                    <button type="submit" className={styles.field}>Enviar</button>
+                    <button type="submit" className={styles.button}>Enviar</button>
                 </div>
             </form>
         </div>
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Formulario from "./Form/Formulario";
+import styles from "../css/register.module.css";
 
 const Register = () => {
     return (
         <>
-            <div className="register">
+            <div className={styles.register}>
                 <Formulario/>
             </div>
         </>
