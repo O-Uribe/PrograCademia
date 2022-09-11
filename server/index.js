9cef48e3f1bf320dd556cddd9e272a03d700c345
Nicolas-2002 <nrojas2020@alu.uct.cl>
2022-09-11 01:55:14 -0300

    Conexion base de datos
    Se hace la conexion a la base de datos de mongodb. y se arregla el servidor, e se borra del packagejson el "type:module" para poder usar require.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,12 @@
-import express from 'express';   
-import morgan from 'morgan';     
-import {Server as SocketServer} from 'socket.io'; 
-import http from 'http';
-import cors from 'cors';
-
-import {PORT} from './config.js';
-
+const http =require('http')
+const cors=require('cors')
+const express = require('express')
+const morgan = require('morgan')
+const SocketServer= require('socket.io')
 
 const app = express();
 const server = http.createServer(app);
-const io = new SocketServer(server, {
+const io =   SocketServer(server, {
     cors: {
         origin: 'http://localhost:3000',
     }
@@ -20,6 +17,7 @@ app.use(cors());
 app.use(morgan('dev')); 
 
 io.on('connection', (socket) => {
+    console.log(socket.id)
     socket.on('message', (message) => {
         socket.broadcast.emit('message', {
             body: message,
@@ -28,7 +26,17 @@ io.on('connection', (socket) => {
     });
 });
 
+//conexion base de datos
+const mongoose = require('mongoose')
+
+const uri = `mongodb+srv://nico2:nico123@cluster0.oxwjx.mongodb.net/Integra_test`;
+mongoose.connect(uri,
+  {useNewUrlParser: true, useUnifiedTopology: true }
+)
+  .then(() => console.log('Base de datos conectada'))
+  .catch(e => console.log(e))
+
 
     
-server.listen(PORT, () => {console.log('Servidor iniciado en el puerto', PORT);});
+server.listen(4000, () => {console.log('Servidor iniciado en el puerto', 4000);});
 
