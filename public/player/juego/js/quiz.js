949f328a350baa1562832355ecda3af51ea4a8e7
Nicolas-2002 <74681749+Nicolas-2002@users.noreply.github.com>
2022-09-20 17:47:58 -0300

    Quiz
    Se implementa el quiz solamente con JavaScript, HTML y CSS. Con sus diferentes funciones.

---
+++ public/player/juego/js/quiz.js
@@ -0,0 +1,185 @@
+//buscamos lo que requerimos
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// Si se quiere realizar el examen
+start_btn.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz"); 
+    showQuetions(0); 
+    queCounter(1); 
+    startTimer(15); 
+    startTimerLine(0); 
+}
+
+let timeValue =  15;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+//Se restablece el quiz
+restart_quiz.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz"); 
+    result_box.classList.remove("activeResult"); 
+    timeValue = 15; 
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuetions(que_count);
+    queCounter(que_numb); 
+    clearInterval(counter); 
+    clearInterval(counterLine);
+    startTimer(timeValue); 
+    startTimerLine(widthValue);
+    timeText.textContent = "Tiempo Faltante"; 
+    next_btn.classList.remove("show");
+}
+
+// Si se preciona salir
+quit_quiz.onclick = ()=>{
+    window.location.reload(); 
+}
+
+
+// Funcion para el boton siguiente donde se comprueba igualmente si es que es la ultima pregunta.
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){ 
+        que_count++; 
+        que_numb++; 
+        showQuetions(que_count); 
+        queCounter(que_numb); 
+        clearInterval(counter); 
+        clearInterval(counterLine); 
+        startTimer(timeValue); 
+        startTimerLine(widthValue); 
+        timeText.textContent = "Tiempo Faltante"; 
+        next_btn.classList.remove("show"); 
+    }else{
+        clearInterval(counter); 
+        clearInterval(counterLine); 
+        showResult(); 
+    }
+}
+
+// mostrar el arreglo con las preguntas
+function showQuetions(index){
+    const que_text = document.querySelector(".que_text");
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; 
+    option_list.innerHTML = option_tag; 
+    
+    const option = option_list.querySelectorAll(".option");
+
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+// nuevos div para los iconos
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+//Cuando se hace clik en las opciones
+function optionSelected(answer){
+    clearInterval(counter); 
+    clearInterval(counterLine); 
+    let userAns = answer.textContent; 
+    let correcAns = questions[que_count].answer; 
+    const allOptions = option_list.children.length; 
+    
+    if(userAns == correcAns){ 
+        userScore += 1; 
+        answer.classList.add("correct"); 
+        answer.insertAdjacentHTML("beforeend", tickIconTag); 
+        console.log("Respuesta Correcta");
+        console.log("Tus respuestas correctas = " + userScore);
+    }else{
+        answer.classList.add("incorrect"); 
+        answer.insertAdjacentHTML("beforeend", crossIconTag); 
+        console.log("Wrong Answer");
+
+        for(i=0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correcAns){ 
+                option_list.children[i].setAttribute("class", "option correct"); 
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); 
+                console.log("Respuesta correcta seleccionada automáticamente.");
+            }
+        }
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled");
+    }
+    next_btn.classList.add("show"); 
+}
+//Se muestra el puntaje obtenido
+function showResult(){
+    quiz_box.classList.remove("activeQuiz"); 
+    result_box.classList.add("activeResult");
+    const scoreText = result_box.querySelector(".score_text");
+    let scoreTag = '<span>Obtuviste <p>'+ userScore +'</p> de <p>'+ questions.length +'</p></span>';
+    scoreText.innerHTML = scoreTag;  
+}
+
+//Timer
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time; 
+        time--; 
+        if(time < 9){ 
+            let addZero = timeCount.textContent; 
+            timeCount.textContent = "0" + addZero; 
+        }
+        if(time < 0){ 
+            clearInterval(counter); 
+            timeText.textContent = "Finalizó Tiempo";
+            const allOptions = option_list.children.length; 
+            let correcAns = questions[que_count].answer; 
+            for(i=0; i < allOptions; i++){
+                if(option_list.children[i].textContent == correcAns){ 
+                    option_list.children[i].setAttribute("class", "option correct"); 
+                    option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); 
+                    console.log("Time Off: Auto selected correct answer.");
+                }
+            }
+            for(i=0; i < allOptions; i++){
+                option_list.children[i].classList.add("disabled"); 
+            }
+            next_btn.classList.add("show"); 
+        }
+    }
+}
+
+//Timer para la linea
+function startTimerLine(time){
+    counterLine = setInterval(timer, 29);
+    function timer(){
+        time += 1; 
+        time_line.style.width = time + "px";
+        if(time > 549){
+            clearInterval(counterLine); 
+        }
+    }
+}
+
+//Muestra en que pregunta vamos.
+function queCounter(index){
+    let totalQueCounTag = '<span><p>'+ index +'</p> de <p>'+ questions.length +'</p> Preguntas</span>';
+    bottom_ques_counter.innerHTML = totalQueCounTag; 
+}
