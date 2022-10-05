0cbf6ee748481013d2bd3a465aea12193ca48c67
Gonzalo <ouribe2020@alu.uct.cl>
2022-10-05 20:26:43 -0300

    Reincorporacion de React
    Se separan nuevamente las partes de front-end y back-end para hacer uso de react y express

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+ 
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
+import Chat from "./components/chat";
+import OffLine from "./components/offline";
+import Player from "./components/player";
+import Game from "./components/game";
+import Host from "./components/host";
+import HostGame from "./components/hostGame";
+import QuizCreator from "./components/quizCreator";
+import LobbyCreator from "./components/lobbyCreator";
+
+function App() {
+    return (
+  
+      <div>
+     <Navbar />
+     <Routes>
+    	<Route exact path="/" element={<RecordList />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/Chat" element={<Chat />} />
+        <Route path="/OffLine" element={<OffLine />} /> 
+        <Route path="/player" element={<Player />} />
+		<Route path="/game" element={<Game />} />
+		<Route path="/host" element={<Host />} />
+		<Route path="/hostGame" element={<HostGame />} /> 
+		<Route path="/quizcreator" element={<QuizCreator />} />
+		<Route path="/lobbycreator" element={<LobbyCreator />} />
+		</Routes>
+   </div>
+    );
+  }
+  
+export default App;
+
+
+
+
+
