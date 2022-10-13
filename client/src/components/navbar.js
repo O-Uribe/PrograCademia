cde0be7243f660cf98e6b8857607cde4c32df89e
Matías Sepúlveda <71105266+sepuuu@users.noreply.github.com>
2022-10-13 01:10:03 -0300

    implementacion tailwind y daisyui

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -8,20 +8,26 @@ import { NavLink } from "react-router-dom";
 // Here, we display our Navbar
 export default function Navbar() {
  return (
-   <div>
-     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-
- 
-       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul className="navbar-nav ml-auto">
-           <li className="nav-item">
-             <NavLink className="nav-link" to="/create">
-               Create Record
-             </NavLink>
-           </li>
-         </ul>
-       </div>
-     </nav>
-   </div>
+<div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal p-0">
+      <li><a>Item 1</a></li>
+      <li tabIndex={0}>
+        <a>
+          Parent
+          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+        </a>
+        <ul className="p-2 bg-base-100">
+          <li><a>Submenu 1</a></li>
+          <li><a>Submenu 2</a></li>
+        </ul>
+      </li>
+      <li><a>Item 3</a></li>
+    </ul>
+  </div>
+</div>
  );
 }
