a7f49264162b9def01bca76efc156797b83fcc34
Nicolas-2002 <nrojas2020@alu.uct.cl>
2022-10-23 17:12:31 -0300

    Pruebas para hace musica
    Solamente son pruebas para hacer un mini reproductor en el cual pueda ser controlado por el profesor, pero escuchado en el alumno y profesor.
    /pruebas  para poder verlo

---
+++ client/src/components/audios.js
@@ -0,0 +1,18 @@
+exports.songsdata = [
+    {
+        "title": "Drake - Forever",
+        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+    },
+    {
+        "title": "Linking Park - In the end",
+        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+    },
+    {
+        "title": "Travis Scott - Stop trina be God",
+        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+    },
+    {
+        "title":"Deep web",
+        "url":"https://dl.last.fm/static/1666555362/133528063/9e6892b2ffc90fb13eedb6797b9bbf5af1f45f8d176e8f9c52c6ad0679a9021c/Death+Grips+-+Deep+Web.mp3"
+    }
+]
--- client/src/pages/pruebas.jsx
+++ client/src/pages/pruebas.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import videoBg from '../assets/videoBg.mp4'
-import { Link } from 'react-router-dom';
-const pruebas = () => {
+import Player from '../components/Player';
+import { songsdata } from '../components/audios';
+import { useRef, useState, useEffect } from 'react';
+
+const App = () => {
+  const [songs, setSongs] = useState(songsdata);
+  const [isplaying, setisplaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(songsdata[1]);
+
+  const audioElem = useRef();
+
+  useEffect(() => {
+    if (isplaying) {
+      audioElem.current.play();
+    }
+    else {
+      audioElem.current.pause();
+    }
+  }, [isplaying])
+
+  const onPlaying = () => {
+    const duration = audioElem.current.duration;
+    const ct = audioElem.current.currentTime;
+
+    setCurrentSong({ ...currentSong, "progress": ct / duration * 100, "length": duration })
+
+  }
+
+
   return (
-    <div className='h-screen w-full'>
-        <div className=" h-full absolute top-0 left-0 w-full"></div>
-        <video src={videoBg} autoPlay loop muted className="h-full object-cover w-full" />
-        <div className="flex flex-col items-center justify-center h-full absolute top-0 text-black w-full">
-        <h1 className="mb-8 text-3xl text-center">Bienvenido a Progracademia</h1>
-              <>
-                <Link to="/home" className="w-full text-center py-3 btn btn-outline bg-orange-700" >Jugar</Link>
-              </>
-        </div>
+    <div className="flex items-center justify-center h-screen w-full">
+      <audio src={currentSong.url} ref={audioElem} onTimeUpdate={onPlaying} />
+      <Player songs={songs} setSongs={setSongs} isplaying={isplaying} setisplaying={setisplaying} audioElem={audioElem} currentSong={currentSong} setCurrentSong={setCurrentSong} />
     </div>
-  )
+  );
 }
 
-export default pruebas
+export default App;
