c87f08398283b9c6b262cabe64c0ff215eb7d69e
niquiprime <71198628+niquiprime@users.noreply.github.com>
2022-11-02 22:44:45 -0300

    Update ShowProfesor.jsx

--- client/src/pages/ShowProfesor.jsx
+++ client/src/pages/ShowProfesor.jsx
@@ -14,27 +14,62 @@ export const ShowProfesor = () => {
 
     //Mostrar Todos los profesores de la Base de Datos
     const [profesores, setProfesores] = React.useState([]);
+    const [busqueda, setBusqueda] = React.useState("");
+    const [tablaUsuarios, setTablaUsuarios] = React.useState([]);
 
     React.useEffect(() => {
         fetch("http://localhost:5000/profesor")
             .then((response) => response.json())
-            .then((data) => setProfesores(data));
+            .then((data) => {
+                setProfesores(data)
+                setTablaUsuarios(data)
+            });
     }, []);
 
     function handleDelete(id) {
-        fetch(`http://localhost:5000/profesor/${id}`, {
+        if(window.confirm("¿Está seguro de eliminar este profesor?")) {
+            fetch(`http://localhost:5000/profesor/${id}`, {
             method: "DELETE",
         }).then((response) => {
             if (response.ok) {
                 alert("Profesor Eliminado");
-                window.location.reload();
+                // Actualizar la lista de Profesores
+                fetch("http://localhost:5000/profesor")
+                    .then((response) => response.json())
+                    .then((data) => setProfesores(data));
             }
         });
+        }
     }
+    //handlechange para buscar por nombre o apellido
+    function handleChange(e) {
+        setBusqueda(e.target.value);
+        filtrar(e.target.value);
+    }
+    const filtrar = (terminoBusqueda) => {
+        var resultadosBusqueda = tablaUsuarios.filter((elemento) => {
+            if (
+                elemento.nombre.toLowerCase().includes(terminoBusqueda.toLowerCase()) ||
+                elemento.apellido.toLowerCase().includes(terminoBusqueda.toLowerCase())
+            ) {
+                return elemento;
+            }
+        });
+        setProfesores(resultadosBusqueda);
+    };
 
     return (
+        <>
         <div class="overflow-x-auto">
             <h1>Profesores</h1>
+            {/* Buscador de Estudiante */}
+            <div className="container">
+                <input type="text" 
+                placeholder="Buscar Profesor por Nombre o Apellido" 
+                onChange={handleChange} 
+                value={busqueda} 
+                style={{color : "black"}}/>
+            </div>
             <table class="table w-full">
                 <thead>
                     <tr>
@@ -59,6 +94,7 @@ export const ShowProfesor = () => {
                 </tbody>
             </table>
         </div>
+        </>
     );
 };
 
