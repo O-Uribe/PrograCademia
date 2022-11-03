8a8e56357df0552c9e0a38902d4c90e4b2477c46
ericx0m <eovando2018@alu.uct.cl>
2022-11-03 03:20:57 -0300

    probando
    buscador

--- client/src/pages/formulario.js
+++ client/src/pages/formulario.js
@@ -8,7 +8,7 @@ const Formulario = () => {
     const estado = UseFetch(url);
     const { cargando, dato } = estado;
 
-    const { register, formState: { errors }, handleSubmit } = useForm(
+    const { register, handleSubmit } = useForm(
         {
             defaultValues: {
                 categoria: 'Science:Computers',
@@ -61,73 +61,80 @@ const Formulario = () => {
 
     return (
         <>
-    
         <center className="absolute">
-            <form class="bg-base-100 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(onSubmit)}>
-                <div class="flex flex-wrap -mx-3 mb-6">
-                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">Categoria</label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        type='text' {...register('categoria')}></input>
-                        <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">Tipo de Pregunta</label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        type='text' {...register('tipo_Pregunta')}></input>
-                    </div>
-                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">Dificultad</label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
-                        type='text' {...register('dificultad')}></input>
-                        <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">Titulo</label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
-                        type='text' {...register('titulo')}></input>
-                    </div>
-                </div>
-                Respuestas
-                <div class="flex flex-wrap -mx-3 mb-6">
-                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">A</label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
-                        type='text' {...register('a')}></input>
-                        <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">B</label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
-                        type='text' {...register('b')}></input>
-                        <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">C</label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
-                        type='text' {...register('c')}></input>
-                        <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">D</label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
-                        type='text' {...register('d')}></input>
-                    </div>
-                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">================</label>
-                        <select class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                                {...register('a2')}>
-                                <option>False</option>
-                                <option>True</option>
-                        </select>
-                        <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">================</label>
-                        <select class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                                {...register('b2')}>
-                                <option>False</option>
-                                <option>True</option>
-                        </select>
-                        <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">================</label>
-                        <select class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                                {...register('c2')}>
-                                <option>False</option>
-                                <option>True</option>
-                        </select>
-                        <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">================</label>
-                        <select class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                                {...register('d2')}>
-                                <option>False</option>
-                                <option>True</option>
-                        </select>
-                    </div>
-                </div>
-                <input class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                type="submit" value="enviar"/>
-            </form>
+            <div>
+                {
+                    cargando
+                    ?
+                    <p>...Cargando</p>
+                    :
+                    <form class="bg-base-100 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(onSubmit)}>
+                        <div class="flex flex-wrap -mx-3 mb-6">
+                            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">Categoria</label>
+                                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                type='text' {...register('categoria')}></input>
+                                <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">Tipo de Pregunta</label>
+                                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                type='text' {...register('tipo_Pregunta')}></input>
+                            </div>
+                            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">Dificultad</label>
+                                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+                                type='text' {...register('dificultad')}></input>
+                                <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">Titulo</label>
+                                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+                                type='text' {...register('titulo')}></input>
+                            </div>
+                        </div>
+                        Respuestas
+                        <div class="flex flex-wrap -mx-3 mb-6">
+                            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">A</label>
+                                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+                                type='text' {...register('a')}></input>
+                                <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">B</label>
+                                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+                                type='text' {...register('b')}></input>
+                                <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">C</label>
+                                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+                                type='text' {...register('c')}></input>
+                                <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">D</label>
+                                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+                                type='text' {...register('d')}></input>
+                            </div>
+                            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">================</label>
+                                <select class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                        {...register('a2')}>
+                                        <option>False</option>
+                                        <option>True</option>
+                                </select>
+                                <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">================</label>
+                                <select class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                        {...register('b2')}>
+                                        <option>False</option>
+                                        <option>True</option>
+                                </select>
+                                <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">================</label>
+                                <select class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                        {...register('c2')}>
+                                        <option>False</option>
+                                        <option>True</option>
+                                </select>
+                                <label class="block uppercase tracking-wide text-white text-xs font-bold mb-2">================</label>
+                                <select class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                        {...register('d2')}>
+                                        <option>False</option>
+                                        <option>True</option>
+                                </select>
+                            </div>
+                        </div>
+                        <input class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                        type="submit" value="enviar"/>
+                    </form>
+                }
+            </div>    
         </center>
         </>
     );
