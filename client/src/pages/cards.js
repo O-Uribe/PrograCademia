e1ad4b3a3b64fe203cc11098b990c67dbcbeb8b1
ericx0m <eovando2018@alu.uct.cl>
2022-11-04 03:40:21 -0300

    buscador en las cartas
    se creo un buscador en las cartas

--- client/src/pages/cards.js
+++ client/src/pages/cards.js
@@ -6,6 +6,14 @@ function Cards() {
     const [url] = useState("https://restapi-progracademia.herokuapp.com/api/preguntas/");
     const estado = UseFetch(url);
     const { cargando, dato } = estado;
+    const [search, setSearch] = useState('');
+
+    const results = !search ? dato : dato.filter((data) => data.titulo.toLowerCase().includes(search.toLocaleLowerCase()))
+
+    const searcher = (e) => {
+        setSearch(e.target.value);
+        console.log(e.target.value);
+    }
 
     return (
         <div>
@@ -14,15 +22,18 @@ function Cards() {
                 ?
                 <p>...Cargando</p>
                 :
-                <div className='container d-flex justify-content-center align-items-center h-100'>
-                    <div className='row'>
-                        {
-                            dato.map(p => (
-                                <div className='col-md-4' key={p._id}>
-                                    <Card titulo={p.titulo} categoria={p.categoria}/>
-                                </div>
-                            ))
-                        }
+                <div>
+                    <input value={search} onChange={searcher} type='text' placeholder='Search' className='form-control'></input>
+                    <div className='container d-flex justify-content-center align-items-center h-100'>
+                        <div className='row'>
+                            {
+                                results.map(p => (
+                                    <div className='col-md-4' key={p._id}>
+                                        <Card titulo={p.titulo} categoria={p.categoria}/>
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
             }
